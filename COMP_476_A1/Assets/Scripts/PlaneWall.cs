11cb6409bab8e50274863876e4a3215c2746c7df
Christian Plourde <christian.bertin.plourde@gmail.com>
2020-01-16 17:23:24 -0500

    first commit

---
+++ COMP_476_A1/Assets/Scripts/PlaneWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneWall : MonoBehaviour
+{
+    public GameObject pair_wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.gameObject.transform.position);
+    }
+}
