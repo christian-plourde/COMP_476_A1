ae657c1f915139862353ce13af9a16d0fa419d08
Christian Plourde <christian.bertin.plourde@gmail.com>
2020-01-23 16:50:01 -0500

    started movement

--- COMP_476_A1/Assets/Scripts/Car.cs
+++ COMP_476_A1/Assets/Scripts/Car.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    public float start_velocity;
+    private const float max_velocity = 0.03f;
     public static float reset_offset = 0.8f;
+    public Movement movement;
+
+    public float MaxVelocity
+    {
+        get { return max_velocity; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement = new KinematicSeek(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + start_velocity);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + MaxVelocity);
+        movement.Move();
     }
 }
---
+++ COMP_476_A1/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Movement
+{
+    private Car car;
+    private Vector3 target;
+
+    public Vector3 Target
+    {
+        get { return target; }
+        set { target = value; }
+    }
+
+    public Vector3 Position
+    {
+        get { return car.gameObject.transform.position; }
+    }
+
+    public Movement(Car car)
+    {
+        this.car = car;
+    }
+
+    public abstract void Move();
+
+}
+
+public class KinematicSeek : Movement
+{
+    public KinematicSeek(Car car) : base(car) { }
+
+    public override void Move()
+    {
+        Debug.Log("Car: " + Position.ToString() + " Target: " + Target.ToString());
+    }
+}
